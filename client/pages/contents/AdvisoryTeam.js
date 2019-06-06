3ee9e3a85841c80dee887084090a831ae366e8f4
waqastanoli <w.tanoli@tap.company>
2019-06-07 04:28:57 +0500

    updates

--- client/pages/contents/AdvisoryTeam.js
+++ client/pages/contents/AdvisoryTeam.js
@@ -5,144 +5,220 @@ export default class AdvisoryTeam extends Component {
   render() {
     return (
       <div className="main logged_in">
-      <div className="page-style paddintop15">
-            <p><strong><span className='heading_how_it_works mg15'>ADVISORY TEAM AND FRIENDS OF POM</span></strong></p>
-<div className="hiw-section">
-<div className="col-sm-12">
-          <div className="vc_row wpb_row vc_row-fluid"><div className="wpb_column vc_column_container vc_col-sm-4"><div className="vc_column-inner "><div className="wpb_wrapper">
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p><strong>C. L. Morci: Founder</strong></p>
-
-    </div>
-  </div>
-
-  <div className="wpb_single_image wpb_content_element vc_align_left">
-    
-    <figure className="wpb_wrapper vc_figure">
-      <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="255" height="300" src="https://restoredvalor.org/wp-content/uploads/2019/06/christine-m.png" className="vc_single_image-img attachment-full" alt=""/></div>
-    </figure>
-  </div>
-
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p>We combine the forces of TV and Internet to create opportunity and outcomes.</p>
-
-    </div>
-  </div>
-</div></div></div><div className="wpb_column vc_column_container vc_col-sm-4"><div className="vc_column-inner "><div className="wpb_wrapper">
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p><strong>Christopher Lemmon</strong></p>
-
-    </div>
-  </div>
-
-  <div className="wpb_single_image wpb_content_element vc_align_left">
-    
-    <figure className="wpb_wrapper vc_figure">
-      <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="244" height="299" src="https://restoredvalor.org/wp-content/uploads/2019/06/chris-lemmon.png" className="vc_single_image-img attachment-full" alt=""/></div>
-    </figure>
-  </div>
-
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p>Multi-talented actor, author, musician and producer (son of Jack Lemmon)</p>
-
-    </div>
-  </div>
-</div></div></div><div className="wpb_column vc_column_container vc_col-sm-4"><div className="vc_column-inner "><div className="wpb_wrapper">
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p><strong>Jeff Spaulding</strong></p>
-
-    </div>
-  </div>
-
-  <div className="wpb_single_image wpb_content_element vc_align_left">
-    
-    <figure className="wpb_wrapper vc_figure">
-      <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="252" height="298" src="https://restoredvalor.org/wp-content/uploads/2019/06/jeff-spaulding.png" className="vc_single_image-img attachment-full" alt=""/></div>
-    </figure>
-  </div>
-
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p>(MLB) TV & Film Director, Exec Producer, consultant ("Babe Ruth", "Derek Jeter", etc.)</p>
-
-    </div>
-  </div>
-</div></div></div></div><div className="vc_row wpb_row vc_row-fluid"><div className="wpb_column vc_column_container vc_col-sm-12"><div className="vc_column-inner "><div className="wpb_wrapper"><div className="vc_empty_space" style={{ height: "50px"}}><span className="vc_empty_space_inner"></span></div>
-</div></div></div></div><div className="vc_row wpb_row vc_row-fluid"><div className="wpb_column vc_column_container vc_col-sm-4"><div className="vc_column-inner "><div className="wpb_wrapper">
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p><strong>Lorraine B. Ling, Esq.</strong><strong><br/>
-</strong></p>
-
-    </div>
-  </div>
-
-  <div className="wpb_single_image wpb_content_element vc_align_left">
-    
-    <figure className="wpb_wrapper vc_figure">
-      <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="244" height="299" src="https://restoredvalor.org/wp-content/uploads/2019/06/lorraine-ling.png" className="vc_single_image-img attachment-full" alt=""/></div>
-    </figure>
-  </div>
-
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p>Patent Attorney, VP corp.: M.S. Biology, M.A English, B.A. Chemistry. Mindfulness certified</p>
-
-    </div>
-  </div>
-</div></div></div><div className="wpb_column vc_column_container vc_col-sm-4"><div className="vc_column-inner "><div className="wpb_wrapper">
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p><strong>Timothy Hurja</strong></p>
-
-    </div>
-  </div>
-
-  <div className="wpb_single_image wpb_content_element vc_align_left">
-    
-    <figure className="wpb_wrapper vc_figure">
-      <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="257" height="297" src="https://restoredvalor.org/wp-content/uploads/2019/06/tim-hurja.png" className="vc_single_image-img attachment-full" alt=""/></div>
-    </figure>
-  </div>
-
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p>PGA Pro. Personal instructor to Tony Robbins. Results specialist TRI</p>
-
-    </div>
-  </div>
-</div></div></div><div className="wpb_column vc_column_container vc_col-sm-4"><div className="vc_column-inner "><div className="wpb_wrapper">
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p><strong>Allison Dollar</strong></p>
-
-    </div>
-  </div>
-
-  <div className="wpb_single_image wpb_content_element vc_align_left">
-    
-    <figure className="wpb_wrapper vc_figure">
-      <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="251" height="298" src="https://restoredvalor.org/wp-content/uploads/2019/06/allison-dollar.png" className="vc_single_image-img attachment-full" alt=""/></div>
-    </figure>
-  </div>
-
-  <div className="wpb_text_column wpb_content_element ">
-    <div className="wpb_wrapper">
-      <p>CEO iTV Alliance of Silicon Valley. Foremost authority of Internet and TV interaction</p>
-
-    </div>
-  </div>
-</div></div></div></div><div className="vc_row wpb_row vc_row-fluid"><div className="wpb_column vc_column_container vc_col-sm-12"><div className="vc_column-inner "><div className="wpb_wrapper"><div className="vc_empty_space" style={{height: "50px"}}><span className="vc_empty_space_inner"></span></div>
-</div></div></div></div>
-        </div>
-                 </div>
+   <div className="page-style paddintop15">
+      <p><strong><span className='heading_how_it_works mg15'>ADVISORY TEAM AND FRIENDS OF POM</span></strong></p>
+      <div className="hiw-section">
+         <div className="col-sm-12">
+            <div className="vc_row wpb_row vc_row-fluid">
+               <div className="wpb_column vc_column_container vc_col-sm-4">
+                  <div className="vc_column-inner ">
+                     <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p><strong>C. L. Morci: Founder</strong></p>
+                           </div>
                         </div>
+                        <div className="wpb_single_image wpb_content_element vc_align_left">
+                           <figure className="wpb_wrapper vc_figure">
+                              <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="255" height="300" src="https://restoredvalor.org/wp-content/uploads/2019/06/christine-m.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                           </figure>
                         </div>
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p>We combine the forces of TV and Internet to create opportunity and outcomes.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="wpb_column vc_column_container vc_col-sm-4">
+                  <div className="vc_column-inner ">
+                     <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p><strong>Christopher Lemmon</strong></p>
+                           </div>
+                        </div>
+                        <div className="wpb_single_image wpb_content_element vc_align_left">
+                           <figure className="wpb_wrapper vc_figure">
+                              <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="244" height="299" src="https://restoredvalor.org/wp-content/uploads/2019/06/chris-lemmon.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                           </figure>
+                        </div>
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p>Multi-talented actor, author, musician and producer (son of Jack Lemmon)</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="wpb_column vc_column_container vc_col-sm-4">
+                  <div className="vc_column-inner ">
+                     <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p><strong>Justin Prust<br/>
+                                 </strong>
+                              </p>
+                           </div>
+                        </div>
+                        <div className="wpb_single_image wpb_content_element vc_align_left">
+                           <figure className="wpb_wrapper vc_figure">
+                              <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="244" height="299" src="https://restoredvalor.org/wp-content/uploads/2019/06/justin-prust.jpg" className="vc_single_image-img attachment-full" alt=""/></div>
+                           </figure>
+                        </div>
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p>Justin Prust,MBA, CEO of Prust Inc., transforms FOrtune 100 companies: implements Robotic Automation (RPA) and trains leaders and teams to motivate and innovate.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <div className="vc_row wpb_row vc_row-fluid">
+               <div className="wpb_column vc_column_container vc_col-sm-4">
+                  <div className="vc_column-inner ">
+                     <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p><strong>Lorraine B. Ling, Esq.</strong><strong><br/>
+                                 </strong>
+                              </p>
+                           </div>
+                        </div>
+                        <div className="wpb_single_image wpb_content_element vc_align_left">
+                           <figure className="wpb_wrapper vc_figure">
+                              <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="244" height="299" src="https://restoredvalor.org/wp-content/uploads/2019/06/lorraine-ling.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                           </figure>
+                        </div>
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p>Patent Attorney, VP corp.: M.S. Biology, M.A English, B.A. Chemistry. Mindfulness certified</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="wpb_column vc_column_container vc_col-sm-4">
+                  <div className="vc_column-inner ">
+                     <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p><strong>Jeff Spaulding</strong></p>
+                           </div>
+                        </div>
+                        <div className="wpb_single_image wpb_content_element vc_align_left">
+                           <figure className="wpb_wrapper vc_figure">
+                              <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="252" height="298" src="https://restoredvalor.org/wp-content/uploads/2019/06/jeff-spaulding.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                           </figure>
+                        </div>
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p>(MLB) TV & Film Director, Exec Producer, consultant ("Babe Ruth", "Derek Jeter", etc.)</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="wpb_column vc_column_container vc_col-sm-4">
+                  <div className="vc_column-inner ">
+                     <div className="wpb_wrapper">
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p><strong>Allison Dollar</strong></p>
+                           </div>
+                        </div>
+                        <div className="wpb_single_image wpb_content_element vc_align_left">
+                           <figure className="wpb_wrapper vc_figure">
+                              <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="251" height="298" src="https://restoredvalor.org/wp-content/uploads/2019/06/allison-dollar.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                           </figure>
+                        </div>
+                        <div className="wpb_text_column wpb_content_element ">
+                           <div className="wpb_wrapper">
+                              <p>CEO iTV Alliance of Silicon Valley. Foremost authority of Internet and TV interaction</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            
+         <div className="vc_row wpb_row vc_row-fluid" style={{float:'left'}}>
+            <div className="wpb_column vc_column_container vc_col-sm-4">
+               <div className="vc_column-inner ">
+                  <div className="wpb_wrapper">
+                     <div className="wpb_text_column wpb_content_element ">
+                        <div className="wpb_wrapper">
+                           <p><strong>Timothy Hurja</strong></p>
+                        </div>
+                     </div>
+                     <div className="wpb_single_image wpb_content_element vc_align_left">
+                        <figure className="wpb_wrapper vc_figure">
+                           <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="257" height="297" src="https://restoredvalor.org/wp-content/uploads/2019/06/tim-hurja.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                        </figure>
+                     </div>
+                     <div className="wpb_text_column wpb_content_element ">
+                        <div className="wpb_wrapper">
+                           <p>PGA Pro. Personal instructor to Tony Robbins. Results specialist TRI</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div className="wpb_column vc_column_container vc_col-sm-4">
+               <div className="vc_column-inner ">
+                  <div className="wpb_wrapper">
+                     <div className="wpb_text_column wpb_content_element ">
+                        <div className="wpb_wrapper">
+                           <p><strong>Craig S. Curcio, PHD<br/>
+                              </strong>
+                           </p>
+                        </div>
+                     </div>
+                     <div className="wpb_single_image wpb_content_element vc_align_left">
+                        <figure className="wpb_wrapper vc_figure">
+                           <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="231" height="300" src="https://restoredvalor.org/wp-content/uploads/2019/06/craig-curcio.png" className="vc_single_image-img attachment-full" alt=""/></div>
+                        </figure>
+                     </div>
+                     <div className="wpb_text_column wpb_content_element ">
+                        <div className="wpb_wrapper">
+                           <p>30 years Executive in Finance &amp; Business Development; business consultant &amp; entrepreneurial coach</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div className="wpb_column vc_column_container vc_col-sm-4">
+   <div className="vc_column-inner ">
+      <div className="wpb_wrapper">
+         <div className="wpb_text_column wpb_content_element ">
+            <div className="wpb_wrapper">
+               <p><strong>Ed Manetta<br/>
+                  </strong>
+               </p>
+            </div>
+         </div>
+         <div className="wpb_single_image wpb_content_element vc_align_left">
+            <figure className="wpb_wrapper vc_figure">
+               <div className="vc_single_image-wrapper   vc_box_border_grey"><img width="237" height="300" src="https://restoredvalor.org/wp-content/uploads/2019/06/ed-manetta.png" className="vc_single_image-img attachment-full" alt=""/></div>
+            </figure>
+         </div>
+         <div className="wpb_text_column wpb_content_element ">
+            <div className="wpb_wrapper">
+               <p>Director of Sports of the Barclay Bank Sports (Staples Arena) Center and Nassau Coliseum, of the East Coast; Executive Producer, Film and Television</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+         </div>
+         
+   </div>
+</div>
+</div>
+</div>
     )
   }
 }
